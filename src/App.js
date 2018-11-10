47ea1d86a05bfabbca05b07347862e5fe27361eb
Erin Miller <erinthered324@gmail.com>
2018-11-10 18:35:54 -0500

    Add API and start filtered list drawer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import locations from './data/locations.json';
 import MapDisplay from './Components/MapDisplay';
+import ListDrawer from './Components/ListDrawer';
+
 
 class App extends Component {
   state = {
@@ -10,6 +12,24 @@ class App extends Component {
     zoom: 13,
     all: locations
   }
+
+  styles = {
+    menuButton: {
+      marginLeft: 10,
+      marginRight: 20,
+      position: "absolute",
+      left: 10,
+      top: 20,
+      background: "white",
+      padding: 10
+    },
+    hide: {
+      display: 'none'
+    },
+    header: {
+      marginTop: "0px"
+    }
+  };
 
   render() {
     return (
--- src/Components/MapDisplay.js
+++ src/Components/MapDisplay.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {Map, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
 
 const MAP_KEY = "AIzaSyA2mdJGb4x_SNspR6ciS36BTyZAsqcpjdM";
+const FS_CLIENT = "QW0HODMVJJQ1GX2HMLPLDUICPSXQZ1J25HK2OSOPI3XAMO5H";
+const FS_SECRET = "MIFSZXJS1JKXIMMGDDPWE0ABRWRTR1RBRANSTA4PQX22PU4F";
+const FS_VERSION = "20180323";
 
 class MapDisplay extends Component {
     state = {
@@ -13,15 +16,14 @@ class MapDisplay extends Component {
         showingInfoWindow: false
     };
 
-    componentDidMount = () => {
-    }
+    componentDidMount = () => {}
 
     mapReady = (props, map) => {
         this.setState({map});
         this.updateMarkers(this.props.locations);
     }
 
-    closeInfoWindow = () =>{
+    closeInfoWindow = () => {
         this.state.activeMarker && this
             .state
             .activeMarker
@@ -29,19 +31,61 @@ class MapDisplay extends Component {
         this.setState({showingInfoWindow: false, activeMarker: null, activeMarkerProps: null});
     }
 
+    getBusinessInfo = (props, data) => {
+        return data
+            .response
+            .venues
+            .filter(item => item.name.includes(props.name) || props.name.includes(item.name));
+    }
+
     onMarkerClick = (props, marker, e) => {
         this.closeInfoWindow();
-        this.setState({showingInfoWindow: true, activeMarker: marker, activeMarkerProps: props});
+
+        let url = `https://api.foursquare.com/v2/venues/search?client_id=${FS_CLIENT}&client_secret=${FS_SECRET}&v=${FS_VERSION}&radius=100&ll=${props.position.lat},${props.position.lng}&llAcc=100`;
+        let headers = new Headers();
+        let request = new Request(url, {
+            method: 'GET',
+            headers
+        });
+
+        let activeMarkerProps;
+        fetch(request)
+            .then(response => response.json())
+            .then(result => {
+                let restaurant = this.getBusinessInfo(props, result);
+                activeMarkerProps = {
+                    ...props,
+                    foursquare: restaurant[0]
+                };
+
+                if (activeMarkerProps.foursquare) {
+                    let url = `https://api.foursquare.com/v2/venues/${restaurant[0].id}/photos?client_id=${FS_CLIENT}&client_secret=${FS_SECRET}&v=${FS_VERSION}`;
+                    fetch(url)
+                        .then(response => response.json())
+                        .then(result => {
+                            activeMarkerProps = {
+                                ...activeMarkerProps,
+                                images: result.response.photos
+                            };
+                            if (this.state.activeMarker) 
+                                this.state.activeMarker.setAnimation(null);
+                            marker.setAnimation(this.props.google.maps.Animation.BOUNCE);
+                            this.setState({showingInfoWindow: true, activeMarker: marker, activeMarkerProps});
+                        })
+                } else {
+                    marker.setAnimation(this.props.google.maps.Animation.BOUNCE);
+                    this.setState({showingInfoWindow: true, activeMarker: marker, activeMarkerProps});
+                }
+            })
     }
 
     updateMarkers = (locations) => {
-        if (!locations)
-        return;
-
+        if (!locations) 
+            return;
         this
-        .state
-        .markers
-        .forEach(marker => marker.setMap(null));
+            .state
+            .markers
+            .forEach(marker => marker.setMap(null));
 
         let markerProps = [];
         let markers = locations.map((location, index) => {
@@ -49,22 +93,23 @@ class MapDisplay extends Component {
                 key: index,
                 index,
                 name: location.name,
-                postion: location.pos,
+                position: location.pos,
                 url: location.url
             };
             markerProps.push(mProps);
 
             let animation = this.props.google.maps.Animation.DROP;
-            let marker = new this.props.google.maps.Marker({
-                position: location.pos,
-                map: this.state.map,
-                animation
-            });
+            let marker = new this
+                .props
+                .google
+                .maps
+                .Marker({position: location.pos, map: this.state.map, animation});
             marker.addListener('click', () => {
                 this.onMarkerClick(mProps, marker, null);
             });
             return marker;
         })
+
         this.setState({markers, markerProps});
     }
 
@@ -80,7 +125,7 @@ class MapDisplay extends Component {
         let amProps = this.state.activeMarkerProps;
 
         return (
-            <Map 
+            <Map
                 role="application"
                 aria-label="map"
                 onReady={this.mapReady}
@@ -89,7 +134,7 @@ class MapDisplay extends Component {
                 style={style}
                 initialCenter={center}
                 onClick={this.closeInfoWindow}>
-                <InfoWindow 
+                <InfoWindow
                     marker={this.state.activeMarker}
                     visible={this.state.showingInfoWindow}
                     onClose={this.closeInfoWindow}>
@@ -100,8 +145,18 @@ class MapDisplay extends Component {
                                 <a href={amProps.url}>See website</a>
                             )
                             : ""}
+                        {amProps && amProps.images
+                            ? (
+                                <div><img
+                                    alt={amProps.name + " food picture"}
+                                    src={amProps.images.items[0].prefix + "100x100" + amProps.images.items[0].suffix}/>
+                                    <p>Image from Foursquare</p>
+                                </div>
+                            )
+                            : ""
+                        }
                     </div>
-                    </InfoWindow>
+                </InfoWindow>
             </Map>
         )
     }
